b7fd6b44707c6cc28fb9544f8bf39e62fca6f510
krisabundiz <abundizruizdanna@gmail.com>
2023-08-31 23:03:30 -0600

    Menú principal

--- Evidencia1.py
+++ Evidencia1.py
@@ -1,1 +1,9 @@
-print('hola')
+def menu_principal():
+    print('-------MENU--------')
+    print('\n(1) Registar nota')
+    print('(2) Consultas y reportes')
+    print('(3) Cancelar nota')
+    print('(4) Salir del sistema')
+    opcion=input('\nIngrese el núnmero del servicio que desea solicitar: ')
+    if opcion == '1':
+        
